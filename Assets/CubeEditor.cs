822a36270dcf85d12be1411580b72e95cbcf60b3
James Teitsworth <63876458+dw-jet@users.noreply.github.com>
2020-04-28 20:22:46 -0500

    Prefab a cube out that prints grid location
    Prefab a cube out of planes so we can control colors of each face. Set up an editor script to force cubes to snap to grid at all times. Then print the location on the grid on top face.

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    [SerializeField][Range(1, 20)] private int gridSize = 10;
+    private TextMesh textMesh;
+    void Update()
+    {
+        SnapToGrid();
+    }
+
+    private void SnapToGrid()
+    {
+        Vector3 snapPos;
+        var pos = transform.position;
+        snapPos.x = Mathf.RoundToInt(pos.x / gridSize) * gridSize;
+        snapPos.y = 0f;
+        snapPos.z = Mathf.RoundToInt(pos.z / gridSize) * gridSize;
+        transform.position = snapPos;
+        PrintLabel(snapPos.x, snapPos.z);
+    }
+
+    private void PrintLabel(float x, float y)
+    {
+        textMesh.text = x/gridSize + ", " + y/gridSize;
+    }
+
+    private void Start()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+    }
+}
