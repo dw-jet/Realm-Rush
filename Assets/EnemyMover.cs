b171195a7d8c5611de95793762629d7f2e4d5463
James Teitsworth <63876458+dw-jet@users.noreply.github.com>
2020-04-29 22:50:17 -0500

    Set up a grid dictionary to store all the blocks
    Now the world has a script that loads blocks from world into Dictionary so we can detect duplicates and have all coordinates in one place for pathfinding.

--- Assets/EnemyMover.cs
+++ Assets/EnemyMover.cs
@@ -8,7 +8,7 @@ public class EnemyMover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(FollowPath());
+        // StartCoroutine(FollowPath());
     }
 
     IEnumerator FollowPath()
---
+++ Assets/Pathfinder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            if (grid.ContainsKey(waypoint.GetGridCoords()))
+            {
+                Debug.LogWarning("Overlapping block" + waypoint);
+            }
+            grid.Add(waypoint.GetGridCoords(), waypoint);
+        }
+
+        print("Loaded " + waypoints.Length + " blocks");
+    }
+}
