a55394f90e2565f27646405a61cfbfa730c5d43d
James Teitsworth <63876458+dw-jet@users.noreply.github.com>
2020-04-29 21:05:04 -0500

    Refactor so nothing depends on editor script
    Move important vars out of editor script and into waypoint so that we can remove the editor script without breaking anything later.

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -1,40 +1,36 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 [ExecuteInEditMode]
 [SelectionBase]
+[RequireComponent(typeof(Waypoint))]
 public class CubeEditor : MonoBehaviour
 {
-    [SerializeField][Range(1, 20)] private int gridSize = 10;
-    private TextMesh textMesh;
+    private Waypoint waypoint;
+
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
+
     void Update()
     {
         SnapToGrid();
+        UpdateLabel();
     }
 
     private void SnapToGrid()
     {
-        Vector3 snapPos;
-        var pos = transform.position;
-        snapPos.x = Mathf.RoundToInt(pos.x / gridSize) * gridSize;
-        snapPos.y = 0f;
-        snapPos.z = Mathf.RoundToInt(pos.z / gridSize) * gridSize;
-        transform.position = snapPos;
-        PrintLabel(snapPos.x, snapPos.z);
+        var gridSize = waypoint.GetGridSize();
+        transform.position = new Vector3(waypoint.GetGridCoords().x, 0f, waypoint.GetGridCoords().y);
     }
 
-    private void PrintLabel(float x, float y)
+    private void UpdateLabel()
     {
-        var labelText = x/gridSize + ", " + y/gridSize;
+        var gridSize = waypoint.GetGridSize();
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        var labelText = waypoint.GetGridCoords().x/gridSize + ", " + waypoint.GetGridCoords().y/gridSize;
         textMesh.text = labelText;
         gameObject.name = labelText;
-    }
-
-    private void Start()
-    {
-        textMesh = GetComponentInChildren<TextMesh>();
     }
 }
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+    private Vector2Int gridPos;
+    const int gridSize = 10;
+
+    public int GetGridSize()
+    {
+        return gridSize;
+    }
+
+    public Vector2Int GetGridCoords()
+    {
+        var pos = transform.position;
+        return new Vector2Int(
+            Mathf.RoundToInt(pos.x / gridSize) * gridSize,
+            Mathf.RoundToInt(pos.z / gridSize) * gridSize
+            );
+    }
     // Start is called before the first frame update
     void Start()
     {
