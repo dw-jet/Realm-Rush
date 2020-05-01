6fa4ba858fe69cebb8a563a31d677b1fba554d75
James Teitsworth <63876458+dw-jet@users.noreply.github.com>
2020-04-30 23:15:16 -0500

    Reorganize and get tower setup
    Tower setup and looking at enemy. Also did some housekeeping.

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    [SerializeField] private Waypoint start;
+    [SerializeField] private Waypoint end;
+    
+    private bool isRunning = true;
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    private Queue<Waypoint> queue = new Queue<Waypoint>();
+    private Waypoint searchCenter;
+    private List<Waypoint> path = new List<Waypoint>();
+    
+    private Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left,
+    };
+
+    private void FormPath()
+    {
+        path.Add(end);
+        var next = end.exploredFrom;
+        while (next != start)
+        {
+            path.Add(next);
+            next = next.exploredFrom;
+        }
+        path.Add(start);
+        path.Reverse();
+    }
+
+    private void BreadthFirstSearch()
+    {
+        queue.Enqueue(start);
+
+        while (queue.Count > 0 && isRunning)
+        {
+            searchCenter = queue.Dequeue();
+            searchCenter.isExplored = true;
+            HaltIfEndFound();
+            ExploreNeighbors();
+        }
+    }
+
+    private void HaltIfEndFound()
+    {
+        if (searchCenter == end)
+        {
+            isRunning = false;
+        }
+    }
+
+    private void ExploreNeighbors()
+    {
+        if (!isRunning) { return; }
+        
+        foreach (Vector2Int direction in directions)
+        {
+            var neighborCoordinates = direction + searchCenter.GetGridCoords();
+            if (grid.ContainsKey(neighborCoordinates))
+            {
+                QueueNewNeighbors(neighborCoordinates);
+            }
+        }
+    }
+
+    private void QueueNewNeighbors(Vector2Int neighborCoordinates)
+    {
+        var neighbor = grid[neighborCoordinates];
+        if (neighbor.isExplored || queue.Contains(neighbor)) return;
+        queue.Enqueue(neighbor);
+        neighbor.exploredFrom = searchCenter;
+    }
+
+    private void ColorStartAndEnd()
+    {
+        start.SetTopColor(Color.green);
+        end.SetTopColor(Color.red);
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            if (grid.ContainsKey(waypoint.GetGridCoords()))
+            {
+                Debug.LogWarning("Overlapping block" + waypoint);
+            }
+            else
+            {
+                grid.Add(waypoint.GetGridCoords(), waypoint);
+            }
+        }
+
+        print("Loaded " + waypoints.Length + " blocks");
+    }
+
+    public List<Waypoint> GetPath()
+    {
+        LoadBlocks();
+        ColorStartAndEnd();
+        BreadthFirstSearch();
+        FormPath();
+        return path;
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Tower : MonoBehaviour
+{
+
+    [SerializeField] private Transform objectToPan;
+    [SerializeField] private Transform targetEnemy;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        objectToPan.LookAt(targetEnemy);
+    }
+}
