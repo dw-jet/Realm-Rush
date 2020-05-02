98aa2441dba99a958b86d23a540fc5575b8aa37d
James Teitsworth <63876458+dw-jet@users.noreply.github.com>
2020-05-01 22:21:40 -0500

    Set up shooting only when enemy is in range

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.IO;
+﻿using System.Collections;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,17 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
 
     [SerializeField] private Transform objectToPan;
     [SerializeField] private Transform targetEnemy;
-    
+    [SerializeField] private float firingRange = 3.0f;
+    [SerializeField] private ParticleSystem projectileParticle;
+
     // Update is called once per frame
     void Update()
     {
-        objectToPan.LookAt(targetEnemy);
+        if (targetEnemy)
+        {
+            objectToPan.LookAt(targetEnemy);
+            FireAtEnemy();
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void FireAtEnemy()
+    {
+        var distanceToEnemy = Vector3.Distance(gameObject.transform.position, targetEnemy.transform.position);
+        Shoot(distanceToEnemy <= firingRange);
+    }
+
+    private void Shoot(bool isActive)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = isActive;
     }
 }
